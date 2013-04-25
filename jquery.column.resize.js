f6a36e78645887e2b6a5fb59a169261c5355603b
Ryan Angeles <ryan.angeles@kitesystems.com>
2013-04-25 18:00:38 +0800

    no need to specify number of columns

--- jquery.column.resize.js
+++ jquery.column.resize.js
@@ -2,16 +2,17 @@
     $.fn.colresize = function(options){
 
         var defaults = {
-            column_count : 2,
-            container_children : '.cell',
-            wrapper : this
+            column_count        : 0,
+            wrapper_class       : 'displayTable',
+            wrapper             : this,
+            specific_children   : false
         }
 
         var options = $.extend(defaults,options);
 
         this.each(function(){
 
-        	options.wrapper.addClass('perRow_'+options.column_count); // main container of divs to be resize
+        	options.wrapper.addClass(options.wrapper_class); // main container of divs to be resize
 
         	wrapper = options.wrapper;
 
@@ -19,19 +20,53 @@
 
 			if(!is_empty){ //check if container is not empty
 
-				var cells 	= $(wrapper).children(options.container_children);
+				var cells           = $(wrapper).children();
+                var index_array     = $.makeArray();
+                var position_top    = 0;
+                
+                cells.each(function(index,dom_ele){
 
-				for(var i = 0; i < cells.length; i+=options.column_count) {
+                    if(index == cells.length-1){
 
-					var arr = $.makeArray();
-					
-					cells.slice(i, i+options.column_count).each(function(){
+                        index_array.push(cells.length);
 
-				    	arr.push($(this).height()); // put all height of each div
+                        position_top = 'stop';
 
-				    }).css('height', Math.max.apply( Math, arr )); // set height of each div per row base on max value from array
+                    }
 
-				}
+                    if($(dom_ele).position().top != position_top){
+
+                        if(position_top != 'stop'){
+
+                            index_array.push(index);
+
+                        }
+
+                        var next_count = 0;
+
+                        $.each(index_array, function(i,val){
+
+                            var arr = $.makeArray();
+
+                            if(index_array[i+1]){
+
+                                cells.slice(next_count, index_array[i+1]).each(function(){
+
+                                    arr.push($(this).height());
+
+                                }).css('height', Math.max.apply( Math, arr ));
+
+                                next_count = index_array[i+1];
+                            }
+
+                        });
+                    }
+
+                    position_top = $(dom_ele).position().top;
+
+                });
+
+                
 				
 			}
 
